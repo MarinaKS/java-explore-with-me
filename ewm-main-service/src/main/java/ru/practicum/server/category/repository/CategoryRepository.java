972ef22266af668ed022b1da36e7ceb8abc642d9
Satori <kotko.ms@yandex.ru>
2024-01-24 02:46:48 +0300

    fix checkstyle

--- ewm-main-service/src/main/java/ru/practicum/server/category/repository/CategoryRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/server/category/repository/CategoryRepository.java
@@ -5,5 +5,6 @@ import ru.practicum.server.category.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByName(String name);
+
     boolean existsByNameAndIdNot(String name, Long categoryId);
 }
