33ccb8ee09517f289a07e38e7d6cee10b80375b1
Satori <kotko.ms@yandex.ru>
2024-01-24 03:13:01 +0300

    fix tests 3

--- ewm-main-service/src/main/java/ru/practicum/server/compilation/dto/CompilationNewDto.java
+++ ewm-main-service/src/main/java/ru/practicum/server/compilation/dto/CompilationNewDto.java
@@ -4,13 +4,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class CompilationNewDto {
-    private List<Long> events;
+    private List<Long> events = new ArrayList<>();
     private boolean pinned;
     @NotBlank
+    @Size(max = 50)
     private String title;
 }
